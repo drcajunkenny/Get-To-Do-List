7e745a17cf0e0049f4f99990e43658f3a3b522bc
Kenny Anderson <drcajunkenny@gmail.com>
2019-01-12 10:46:08 -0700

    Initial Commit

---
+++ GetToDoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetToDoey
+//
+//  Created by Kenny Anderson on 1/12/19.
+//  Copyright © 2019 Kosmic Boo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
