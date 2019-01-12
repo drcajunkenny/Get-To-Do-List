515a8cc51f5cb3aad8f4ffb39a0915c9141583e1
Kenny Anderson <drcajunkenny@gmail.com>
2019-01-12 14:47:05 -0700

    add new item func complete

--- GetToDoey/TodoListViewController.swift
+++ GetToDoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["go", "buy", "make"]
+    var itemArray = ["go", "buy", "make"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,4 +46,32 @@ class TodoListViewController: UITableViewController {
         
     }
 
+    //Mark - Add New Items
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item Catorgory", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
